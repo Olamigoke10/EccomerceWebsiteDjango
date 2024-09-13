375f3bd81686d990a20511369065b8dcd40405ed
MRBOX <olamigokeadebayo90@gmail.com>
2024-09-13 02:19:15 +0100

    Styling the admin using Jazzmin

--- eccomprj/settings.py
+++ eccomprj/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
     
     # Custom apps
     'core', 
+    'userauths',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +137,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+JAZZMIN_SETTINGS = {
+    'site_header': "Goke's shop",
+    "site_brand": "We deliver",
+    # "site_logo"
+    "copyright": "goke-shop.com"
+}
