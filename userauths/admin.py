910252378e9374ef612c3d5b0b40240fb7027606
MRBOX <olamigokeadebayo90@gmail.com>
2024-09-13 14:12:09 +0100

    User regsitration

--- userauths/admin.py
+++ userauths/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from userauths.models import User
 
 # Register your models here.
-admin.site.register(User)
+class Useradmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'bio']
+            
+admin.site.register(User, Useradmin)
--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from userauths.forms import UserRegisterForm
 
 # Create your views here.
+def register_view(request):
+    if request.method == "POST":
+        print("USer registered successfully")
+    else:
+        print("USer cannot be registered")
+    form = UserRegisterForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'userauths/signup.html', context)
