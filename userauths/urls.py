f62480f33d7e61621a81c5d2cc221b1641410e92
MRBOX <olamigokeadebayo90@gmail.com>
2024-09-19 10:15:40 +0100

    User login

--- userauths/urls.py
+++ userauths/urls.py
@@ -4,5 +4,7 @@ from userauths import views
 app_name = 'userauths'
 
 urlpatterns = [
-    path("sign-up/", views.register_view, name='sign-up')
+    path("sign-up/", views.register_view, name='signup'),
+    path("sign-in/", views.login_view, name='signin'),
+    path("log-out/", views.logout_view, name='logout')
 ]
--- userauths/views.py
+++ userauths/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import redirect, render
 from userauths.forms import UserRegisterForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 # Create your views here.
 def register_view(request):
@@ -26,3 +29,37 @@ def register_view(request):
         'form': form,
     }
     return render(request, 'userauths/signup.html', context)
+
+def login_view(request):
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged in")
+        return redirect("core:index")
+    
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        try: 
+           user = User.objects.get(email=email)
+        except:
+            messages.warning(request, f"User with {email} does not exist")
+        
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are logged in")
+            return redirect("core:index")
+        else:
+            messages.warning(request, "USer does not exists, Create an Account")
+        
+        context = {
+            
+        }
+            
+    return render(request, "userauth/signup.html", context)
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You have logged out")
+    return redirect("userauth:signin")
